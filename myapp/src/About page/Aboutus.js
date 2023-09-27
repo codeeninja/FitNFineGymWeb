009ed43fb756ee9e28caeb6362d6cfec11910333
codeeninja <102271798+codeeninja@users.noreply.github.com>
2023-09-27 11:51:39 +0530

    ui\ changes

--- myapp/src/About page/Aboutus.js
+++ myapp/src/About page/Aboutus.js
@@ -6,31 +6,45 @@ import Footer from "../Footer/Footer";
 import BmiCalculator from "../BMI CALCULATOR/BmiCalculator";
 export default function Aboutus() {
   return (
-    <div className="main-screen">
-      <Navbar className="btn-3" bg="dark" expand="lg">
-        <Navbar.Brand className="gym-name mx-4" as={Link} to="/">
-          Fit N Fine Gym
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto mx-1 ">
-            <Link className="nav-link text-light" to="/">
-              Home
-            </Link>
-            <Link className="nav-link text-light" to="/about">
-              About us
-            </Link>
-            <Link className="nav-link text-light" to="/Contactus">
-              Contact us
-            </Link>
-            <Link className="nav-link text-light" to="/login">
-              Login
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <div className="row g-0">
-        <div className="col-12 col-sm-12 col-md-12 text-center pt-2">
+   <>
+      <div>
+        <div>
+          <Navbar
+            className="btn-3"
+            bg="dark"
+            expand="lg"
+            style={{
+              backgroundColor: "#555555",
+              padding: "12px",
+              width: "100%",
+              zIndex: "1000",
+            }}
+          >
+            <Navbar.Brand className="gym-name mx-4" as={Link} to="/">
+              <h6>Fit n Fine Gym</h6>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto mx-1 ">
+                <Link className="nav-link text-light" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link text-light" to="/about">
+                  About us
+                </Link>
+                <Link className="nav-link text-light" to="/Contactus">
+                  Contact us
+                </Link>
+                <Link className="nav-link text-light" to="/login">
+                  Login
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+      <div className="row  mt-4">
+        <div className=" col-md-12 text-center">
           <img
             style={{ maxHeight: "380px" }}
             src="assets/gym2.jpg"
@@ -38,11 +52,12 @@ export default function Aboutus() {
           />
         </div>
       </div>
-      <div className="row g-0">
+      <div className="container">
+      <div className="row mt-3">
         <h1 className="text-center text-white mt-2 mb-3">About us</h1>
-        <div className="col-10 col-md-6 col-sm-8 col-lg-6 mx-auto aboutDiscription pt-2" style={{marginLeft:"1%"}}>
+        <div className="col-md-6 mx-auto">
           <h3
-            className="text-white col-11 col-md-10 col-sm-10 col-lg-6 mx-auto mt-2"
+            className="text-white mt-4"
             style={{ fontSize: "15px" }}
           >
             Our gym was established in 2019 with the aim of helping people
@@ -56,17 +71,19 @@ export default function Aboutus() {
             healthy eating habits and achieve a balanced lifestyle.
           </h3>
         </div>
-        <div className="col-10 col-sm-8 col-md-6 mt-2 col-lg-5 mx-auto">
+        <div className=" col-md-6 mx-auto mb-4">
           <img
-            style={{ maxWidth:"100%",maxHeight:"88%" }}
+            style={{ maxWidth:"100%"}}
             src="assets/gymaboutsection.jpg"
             alt=""
           />
         </div>
       </div>
-      <div className="row g-0">
+      </div>
+      
+      <div className="row">
         <h5 className="text-center text-white pt-2 mt-3">JOIN US NOW</h5>
-        <div className="joinusDiscription text-center text-white pb-2 bold">
+        <div className="col-12 text-center text-white pb-2 bold">
           <h2>
             JOIN US OUR FREE WORKOUT TRAINING <br />
             WITH FIT N FINE GYM
@@ -76,12 +93,17 @@ export default function Aboutus() {
         <div className="col">hi2</div>
         <div className="col">hi3</div> */}
       </div>
-      <div className="row bg-dark mt-4  g-0 text-white">
+   
+      <div
+        className=" bg-dark mt-4 mx-auto text-white"
+        style={{ width: "100%" }}
+        >
         <BmiCalculator />
       </div>
-      <div className="footer mt-4">
-        <Footer />
+      <div className=" mt-4 mx-auto" >
+        <Footer style={{ padding: "12px",
+              width: "100%" }}/>
       </div>
-    </div>
+</>
   );
 }
--- myapp/src/Gym.js
+++ myapp/src/Gym.js
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { Navbar, NavDropdown, Nav } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { Link, useNavigate } from "react-router-dom";
 import Footer from "./Footer/Footer";
 import BmiCalculator from "./BMI CALCULATOR/BmiCalculator";
 
-export default function Gym({title}) {
+export default function Gym({ title }) {
   const navigate = useNavigate();
   const [gymData, setGymData] = useState([]);
   const [cardioData, setcardioData] = useState([]);
@@ -49,187 +41,194 @@ export default function Gym({title}) {
   }, []);
   return (
     <>
-      <div className="main-screen">
-        <Navbar className="btn-3" bg="dark" expand="lg">
-          <Navbar.Brand className="gym-name mx-4" as={Link} to="/">
-            {title}
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto mx-1 ">
-              <Link className="nav-link text-light" to="/">
-                Home
-              </Link>
-              <Link className="nav-link text-light" to="/about">
-                About us
-              </Link>
-              <Link className="nav-link text-light" to="/Contactus">
-                Contact us
-              </Link>
-              <Link className="nav-link text-light" to="/login">
-                Login
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-
-        <div className="main-div">
-          <div className="row g-0">
-            <div className="col-12 col-sm-12 col-md-12 col-lg-8 mt-2 mx-auto">
-              <img className="min-div-img m-1" src="assets/gym1.jpg" alt="" />
-            </div>
-            <div className="col-12 col-sm-12 col-md-12 col-lg-4 text-center pt-5">
-              <h1 style={{ color: "white", width: "98%" }}>Fit n Fine Gym</h1>
-              {userName && <h2>Welcome, {userName}!</h2>}
-              <h6
-                style={{ width: "90%" }}
-                className="pt-4 mx-auto gym-intro text-center"
-              >
-                Welcome to our gym! We are thrilled to have you join our
-                community of fitness enthusiasts. Whether you are a seasoned
-                athlete or just starting out on your fitness journey, we are
-                here to support and guide you every step of the way. Our team of
-                experienced trainers and staff are dedicated to helping you
-                achieve your fitness goals and living your best life. Thank you
-                for choosing us, and we can't wait to see you reach your full
-                potential.
-              </h6>
-            </div>
+      <div>
+        <div>
+          <Navbar
+            className="btn-3"
+            bg="dark"
+            expand="lg"
+            style={{
+              backgroundColor: "#555555",
+              padding: "12px",
+              width: "100%",
+              zIndex: "1000",
+            }}
+          >
+            <Navbar.Brand className="gym-name mx-4" as={Link} to="/">
+              {title}
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto mx-1 ">
+                <Link className="nav-link text-light" to="/">
+                  Home
+                </Link>
+                <Link className="nav-link text-light" to="/about">
+                  About us
+                </Link>
+                <Link className="nav-link text-light" to="/Contactus">
+                  Contact us
+                </Link>
+                <Link className="nav-link text-light" to="/login">
+                  Login
+                </Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+      </div>
+      <div className="m-3 mt-4">
+        <div className="row ">
+          <div className="col-md-6">
+            <img className="min-div-img" src="assets/gym1.jpg" alt="" />
           </div>
-          <div className="container">
+          <div className="col-md-6 ">
+            <h1 className="text-center" style={{ color: "white" }}>
+              Fit n Fine Gym
+            </h1>
+            {userName && <h2>Welcome, {userName}!</h2>}
+            <h6 className="mt-4 text-white text-center ">
+              Welcome to our gym! We are thrilled to have you join our community
+              of fitness enthusiasts. Whether you are a seasoned athlete or just
+              starting out on your fitness journey, we are here to support and
+              guide you every step of the way. Our team of experienced trainers
+              and staff are dedicated to helping you achieve your fitness goals
+              and living your best life. Thank you for choosing us, and we can't
+              wait to see you reach your full potential.
+            </h6>
+          </div>
+        </div>
+        <div className="container">
+          <h1 style={{ color: "white" }} className="text-center mt-3">
+            Packages we Offer For Strength
+          </h1>
+          <div className="row">
+            {gymData.map((plan) => (
+              <div className="mt-3 col-sm-6" key={plan.id}>
+                <div className="card bg-dark text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">{plan.plan}</h4>
+                    <h6 className="card-subtitle mb-2">Price: {plan.price}</h6>
+                    <p className="card-text">{plan.description}</p>
+                    <button
+                      className="activebtn btn btn-outline-primary"
+                      onClick={() => navigate("/login")}
+                    >
+                      Go to Login
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+
             <h1 style={{ color: "white" }} className="text-center mt-3">
-              Packages we Offer For Strength
+              Packages we Offer For Cardio
             </h1>
-            <div className="row">
-              {gymData.map((plan) => (
-                <div className="mt-3 col-12 col-sm-6 col-lg-4" key={plan.id}>
-                  <div className="card bg-dark text-white">
-                    <div className="card-body">
-                      <h4 className="card-title">{plan.plan}</h4>
-                      <h6 className="card-subtitle mb-2">
-                        Price: {plan.price}
-                      </h6>
-                      <p className="card-text">{plan.description}</p>
-                      <button
-                        className="activebtn btn btn-outline-primary"
-                        onClick={() => navigate("/login")}
-                      >
-                        Go to Login
-                      </button>
-                    </div>
+            {cardioData.map((data) => (
+              <div className="mt-3 col-sm-6" key={data.id}>
+                <div className="card bg-dark text-white">
+                  <div className="card-body">
+                    <h4 className="card-title">{data.plan}</h4>
+                    <h6 className="card-subtitle mb-2">Price:-{data.price}</h6>
+                    <p className="card-text">{data.description}</p>
+                    <button
+                      onClick={() => navigate("/login")}
+                      className="activebtn btn btn-outline-primary"
+                    >
+                      Active Now
+                    </button>
                   </div>
                 </div>
-              ))}
-
-              <h1 style={{ color: "white" }} className="text-center mt-3">
-                Packages we Offer For Cardio
-              </h1>
-              {cardioData.map((data) => (
-                <div className="mt-3 col-12 col-sm-6 col-lg-4" key={data.id}>
-                  <div className="card bg-dark text-white">
-                    <div className="card-body">
-                      <h4 className="card-title">{data.plan}</h4>
-                      <h6 className="card-subtitle mb-2">
-                        Price:-{data.price}
-                      </h6>
-                      <p className="card-text">{data.description}</p>
-                      <button
-                        onClick={() => navigate("/login")}
-                        className="activebtn btn btn-outline-primary"
-                      >
-                        Active Now
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
+              </div>
+            ))}
           </div>
-          <div style={{ width: "60%" }} className="testmonial pt-4 m-auto">
-            <h1
-              style={{ textAlign: "center", color: "white" }}
-              className="mb-3"
-            >
-              Testimonial
-            </h1>
-            <Carousel style={{ marginTop: "10px" }}>
-              <Carousel.Item>
-                <img
-                  style={{ height: "280px" }}
-                  className="d-block w-100"
-                  src="assets/gym1.jpg"
-                  alt="First slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  style={{ height: "280px" }}
-                  className="d-block w-100"
-                  src="assets/gym2.jpg"
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  style={{ height: "280px" }}
-                  className="d-block w-100"
-                  src="assets/gym1.jpg"
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-              <Carousel.Item>
-                <img
-                  style={{ height: "280px" }}
-                  className="d-block w-100"
-                  src="assets/gym1.jpg"
-                  alt="Second slide"
-                />
-              </Carousel.Item>
-            </Carousel>
+        </div>
+        <div className="container testmonial mt-4 m-auto">
+          <div className="row">
+          <h1 style={{ textAlign: "center", color: "white" }} className="mb-3">
+            Testimonial
+          </h1>
           </div>
-          <div className="vg-page page-funfact ">
-            <div
-              className="container-fluid bg-dark text-white mt-4 text-center"
-              style={{ width: "95%" }}
-            >
-              <div className="row mx-auto section-counter">
-                <div className="col-md-6 col-lg-3 py-4 wow fadeIn">
-                  <h1 className="number" data-number="768">
-                    768
-                  </h1>
-                  <span>Clients</span>
-                </div>
-                <div className="col-md-6 col-lg-3 py-4 wow fadeIn">
-                  <h1 className="number" data-number="230">
-                    230
-                  </h1>
-                  <span>Project Compleate</span>
-                </div>
-                <div className="col-md-6 col-lg-3 py-4 wow fadeIn">
-                  <h1 className="number" data-number="97">
-                    97
-                  </h1>
-                  <span>Project Ongoing</span>
-                </div>
-                <div className="col-md-6 col-lg-3 py-4 wow fadeIn">
-                  <h1 className="number" data-number="699">
-                    699
-                  </h1>
-                  <span>Client Satisfaction</span>
-                </div>
+          <div className="col-12 mx-aut0">
+          <Carousel style={{ marginTop: "10px" }}>
+            <Carousel.Item>
+              <img
+              style={{ height: "280px" }}
+                className="d-block w-100"
+                src="assets/gym1.jpg"
+                alt="First slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ height: "280px" }}
+                className="d-block w-100"
+                src="assets/gym2.jpg"
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ height: "280px" }}
+                className="d-block w-100"
+                src="assets/gym1.jpg"
+                alt="Second slide"
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                style={{ height: "280px" }}
+                className="d-block w-100"
+                src="assets/gym1.jpg"
+                alt="Second slide"
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+        </div>
+        <div className="vg-page page-funfact ">
+          <div
+            className="container-fluid bg-dark text-white mt-4 text-center"
+            style={{ width: "100%" }}
+          >
+            <div className="row mx-auto section-counter">
+              <div className="col-md-3 py-4 wow fadeIn">
+                <h1 className="number" data-number="768">
+                  768
+                </h1>
+                <span>Clients</span>
+              </div>
+              <div className="col-md-3 py-4 wow fadeIn">
+                <h1 className="number" data-number="230">
+                  230
+                </h1>
+                <span>Project Compleate</span>
+              </div>
+              <div className="col-md-3  py-4 wow fadeIn">
+                <h1 className="number" data-number="97">
+                  97
+                </h1>
+                <span>Project Ongoing</span>
+              </div>
+              <div className="col-md-3  py-4 wow fadeIn">
+                <h1 className="number" data-number="699">
+                  699
+                </h1>
+                <span>Client Satisfaction</span>
               </div>
             </div>
           </div>
         </div>
         <div
-          className="row bg-dark mt-4 mx-auto text-white"
-          style={{ width: "95%" }}
+        className=" bg-dark mt-4 mx-auto text-white"
+        style={{ width: "100%" }}
         >
-          <BmiCalculator />
-        </div>
-        <div className="footer mt-4 mx-auto" style={{ width: "95%" }}>
-          <Footer />
-        </div>
+        <BmiCalculator />
+      </div>
+      <div className=" mt-4 mx-auto" >
+        <Footer style={{ padding: "12px",
+              width: "100%" }}/>
+      </div>
       </div>
     </>
   );
